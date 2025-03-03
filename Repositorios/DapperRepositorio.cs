352adaf379fa2f82f2105136b506611fa811071e
Adilson Cambamba <adilson.cambamba@kiamisoft.ao>
2025-03-03 20:12:14 +0100

    GetConnectionString

--- Repositorios/DapperRepositorio.cs
+++ Repositorios/DapperRepositorio.cs
@@ -1,61 +1,66 @@
 ﻿using Dapper;
 using Microsoft.Data.SqlClient;
-using System.Xml.Linq;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace API_VariasBDs.Repositorios
 {
     public class DapperRepositorio
     {
-        public static void Execute(string dbName, string sql, DynamicParameters parameters = null)
+        private static string GetConnectionString(string dbName)
         {
             string connectionString = GetAppSetting($"ConnectionStrings:{dbName}");
+            if (string.IsNullOrEmpty(connectionString))
+                throw new Exception($"{dbName} não existe em ConnectionStrings");
+            return connectionString;
+        }
 
-            using (SqlConnection connection = new SqlConnection(connectionString))
+        private static SqlConnection GetOpenConnection(string connectionString)
+        {
+            var connection = new SqlConnection(connectionString);
+            connection.Open();
+            return connection;
+        }
+
+        public static void Execute(string dbName, string sql, DynamicParameters parameters = null)
+        {
+            string connectionString = GetConnectionString(dbName);
+
+            using (var connection = GetOpenConnection(connectionString))
             {
-                connection.Open();
                 connection.Execute(sql, parameters);
-                connection.Close();
             }
         }
 
         public static List<T> Query<T>(string dbName, string sql, DynamicParameters parameters = null)
         {
-            List<T> result = new List<T>();
+            string connectionString = GetConnectionString(dbName);
 
-            // Obtém a connection string do appsettings.json
-            string connectionString = GetAppSetting($"ConnectionStrings:{dbName}");
-            if (string.IsNullOrEmpty(connectionString))
-                throw new Exception($"{dbName} não existe em ConnectionStrings");
-
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (var connection = GetOpenConnection(connectionString))
             {
-                connection.Open();
-                result = connection.Query<T>(sql, parameters).ToList();
-                connection.Close();
+                return connection.Query<T>(sql, parameters).ToList();
             }
-
-            return result;
         }
 
         public static T SingleOrDefault<T>(string dbName, string sql, DynamicParameters parameters = null)
         {
-            T result = default;
-            string connectionString = GetAppSetting($"ConnectionStrings:{dbName}");
+            string connectionString = GetConnectionString(dbName);
 
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (var connection = GetOpenConnection(connectionString))
             {
-                connection.Open();
-                result = connection.QuerySingleOrDefault<T>(sql, parameters);
-                connection.Close();
+                return connection.QuerySingleOrDefault<T>(sql, parameters);
             }
-
-            return result;
         }
 
         public static string GetAppSetting(string value)
         {
-            IConfiguration conf = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
-            return (conf[value] ?? "");
+            IConfiguration conf = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            return conf[value] ?? "";
         }
     }
 }
